caf2575282ee01d83ea0feb53c8a17fc77913d15
atzulf <atzulfikar45@gmail.com>
2023-04-05 10:02:09 +0700

    menambah latihan 1,latihan 1.2,latihan 2

---
+++ modul 8/latihan1.2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <title>Latihan 1.2</title>
+    </head>
+
+    <body>
+        <p> Kode <?php echo "PHP";?> di HTML </p>
+    </body>
+
+</html>
