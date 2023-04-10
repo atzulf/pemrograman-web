df7e3e1030d0324a75e98387f86dedb71be321ee
atzulf <atzulfikar45@gmail.com>
2023-04-10 09:50:39 +0700

    menambah latihan 2 latihan 3

---
+++ modul 8/latihan2.2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>Latihan 2.2</title>
+    </head>
+
+    <body>
+        <?php
+        // Deklarasi dan inisialisasi
+            $bil = 3;
+        // Dumping informasi mengenai variabel
+        var_dump($bil);
+            print_r($bil);
+        ?>
+    </body>
+
+</html>
---
+++ modul 8/latihan3.1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <title>Cek Tipe data dan casting</title>
+        </head>
+
+        <body>
+            <?php
+            $bil = 3;
+                var_dump(is_int($bil));
+            // Output: bool(true)
+            $var = "";
+                var_dump(is_string($var));
+            // Output: bool(true)
+            ?>
+        </body>
+
+    </html>
