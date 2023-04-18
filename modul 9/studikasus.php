fbccf520f431bf3163793d79dd2dcf3ec10c1981
atzulf <atzulfikar45@gmail.com>
2023-04-18 22:50:25 +0700

    lagi

--- modul 9/studikasus.php
+++ modul 9/studikasus.php
@@ -5,12 +5,6 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
 rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
 crossorigin="anonymous">
-	<style>
-		.edittipis {
-			font-family: Trebuchet MS;
-			font-size: 20px;
-		}
-	</style>
 
 	<title>Login Form</title>
 	<!-- Merupakan Script Javascript -->
@@ -34,25 +28,29 @@ crossorigin="anonymous">
 	</script>
 </head>
 <body>
-	<div class="container edittipis text-center col-12 col-md8 col-lg-3">
+	<div class="container text-center col-12 col-md8 col-lg-3">
 		<div class="card" style="margin-top: 15%; margin-bottom: 60px;">
 			<h3 style="margin-top: 18px; margin-bottom: 15px;">SIGN IN</h3>
 		<form name="loginForm" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" onsubmit="return validateForm();">
 		<label for="id">ID/Username:</label>
-		<input class="form-control" type="text" id="id" name="id" placeholder="masukkan id/username"><br><br>
+		<input class="form-control" type="text" id="id" name="id"><br><br>
 		<label for="password">Password:</label>
-		<input class="form-control" type="password" id="password" name="password" placeholder="masukkan password"><br><br>
+		<input class="form-control" type="password" id="password" name="password"><br><br>
 		<input type="submit" value="Login" style="margin-top: 20px; margin-bottom: 30px;">
-		</form> 
+	</form>
 		</div>
 	</div>
 	
 	<?php
 	if (isset($_POST["nama"]) || isset($_POST['password'])) {
-		
+		if (!is_string($id) || !is_string($password)) {
+			echo "Data yang dimasukkan harus berupa string.";
+		} else if ($id == "admin" && $password == "123456") {
+			echo "<p>Selamat datang, $id!</p>";
+		} else {
+			echo "<center><p>Login gagal. Silahkan coba lagi.</p><center>";
+		}
 	}
-		
-	
 	?>
 </body>
 </html>
