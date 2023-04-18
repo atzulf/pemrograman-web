c0ed15f80b986edd688443430ae84e7beb0c0431
atzulf <atzulfikar45@gmail.com>
2023-04-18 21:20:43 +0700

    fix studi kasus lagi

--- modul 9/studikasus.php
+++ modul 9/studikasus.php
@@ -5,6 +5,12 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
 rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
 crossorigin="anonymous">
+	<style>
+		.edittipis {
+			font-family: Trebuchet MS;
+			font-size: 20px;
+		}
+	</style>
 
 	<title>Login Form</title>
 	<!-- Merupakan Script Javascript -->
@@ -28,31 +34,25 @@ crossorigin="anonymous">
 	</script>
 </head>
 <body>
-	<div class="container text-center col-12 col-md8 col-lg-3">
+	<div class="container edittipis text-center col-12 col-md8 col-lg-3">
 		<div class="card" style="margin-top: 15%; margin-bottom: 60px;">
-			<h3 style="margim-top: 18px; margin-bottom: 15px;">SIGN IN</h3>
-			<form name="loginForm" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" onsubmit="return validateForm();">
+			<h3 style="margin-top: 18px; margin-bottom: 15px;">SIGN IN</h3>
+		<form name="loginForm" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" onsubmit="return validateForm();">
 		<label for="id">ID/Username:</label>
-		<input type="text" id="id" name="id"><br><br>
+		<input class="form-control" type="text" id="id" name="id" placeholder="masukkan id/username"><br><br>
 		<label for="password">Password:</label>
-		<input type="password" id="password" name="password"><br><br>
-		<input type="submit" value="Login">
-	</form>
+		<input class="form-control" type="password" id="password" name="password" placeholder="masukkan password"><br><br>
+		<input type="submit" value="Login" style="margin-top: 20px; margin-bottom: 30px;">
+		</form> 
 		</div>
 	</div>
 	
 	<?php
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		$id = $_POST["id"];
-		$password = $_POST["password"];
-		if (!is_string($id) || !is_string($password)) {
-			echo "Data yang dimasukkan harus berupa string.";
-		} else if ($id == "admin" && $password == "123456") {
-			echo "<p>Selamat datang, $id!</p>";
-		} else {
-			echo "<center><p>Login gagal. Silahkan coba lagi.</p><center>";
-		}
+	if (isset($_POST["nama"]) || isset($_POST['password'])) {
+		
 	}
+		
+	
 	?>
 </body>
 </html>
